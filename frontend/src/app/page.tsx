620d73a2d86510e2c14f060f33bfe4ded6aced5b
Keshav Dayal <keshavdayal205@gmail.com>
2025-03-15 15:39:09 +0530

    new

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { CouponDashboard } from "@/components/coupon-dashboard";
 
-const API_URL = "https://backend-4u6byxcjq-keshxvdayals-projects.vercel.app"; // ✅ Backend running on 3000
+const API_URL = "https://backend-2pma1y47h-keshxvdayals-projects.vercel.app"; // ✅ Backend running on 3000
 
 export default function Home() {
   const [coupons] = useState<string[]>(["Coupon1", "Coupon2", "Coupon3"]);
